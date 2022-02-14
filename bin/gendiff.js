1f58b4f8e77591c39985987fd81edcb2ce0d145a
Matheria <sensenevercame@gmail.com>
2022-02-15 01:03:49 +0500

    feat: step two

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,10 @@ const program = new Command();
 program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>',  'output format')
+    .action((filepath1, filepath2) => {
+        console.log(filepath1, filepath2)
+    })
 
 program.parse();
