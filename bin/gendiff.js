744c8f17bed4a831e8657f55cc75249d4af1f6b4
Matheria <sensenevercame@gmail.com>
2022-04-08 00:27:53 +0500

    fix linter errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,12 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>',  'output format')
-    .action((filepath1, filepath2) => {
-        console.log(filepath1, filepath2)
-    })
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(filepath1, filepath2);
+  });
 
 program.parse();
