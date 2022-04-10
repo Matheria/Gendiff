2c0e59ced81f8b5b5135b69b4c7e806ba69419b4
Matheria <sensenevercame@gmail.com>
2022-04-10 13:32:05 +0500

    refactor genDiff function

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
+import fileParse from './parsers.js';
 
 const readFile = (filepath) => {
   const fullPath = path.resolve(process.cwd(), filepath);
@@ -10,25 +11,28 @@ const readFile = (filepath) => {
 };
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = JSON.parse(readFile(filepath1));
-  const data2 = JSON.parse(readFile(filepath2));
-  const keys = Object.keys({ ...data1, ...data2 }).sort();
+  const fileExtension1 = path.extname(filepath1);
+  const fileExtension2 = path.extname(filepath2);
+  const parsedFile1 = fileParse(readFile(filepath1), fileExtension1);
+  const parsedFile2 = fileParse(readFile(filepath2), fileExtension2);
+  const keys = Object.keys({ ...parsedFile1, ...parsedFile2 }).sort();
+
   let result = '{\n';
 
   keys.forEach((key) => {
-    const key1 = data1[key];
-    const key2 = data2[key];
+    const key1 = parsedFile1[key];
+    const key2 = parsedFile2[key];
 
     if (key1 === key2) {
       result += `    ${key}: ${key1}\n`;
       return;
     }
 
-    if (_.has(data1, key)) {
+    if (_.has(parsedFile1, key)) {
       result += `  - ${key}: ${key1}\n`;
     }
 
-    if (_.has(data2, key)) {
+    if (_.has(parsedFile2, key)) {
       result += `  + ${key}: ${key2}\n`;
     }
   });
