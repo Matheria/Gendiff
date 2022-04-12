0796bf9c2484684006c0b475f39bde84bdbe1553
Matheria <sensenevercame@gmail.com>
2022-04-13 01:33:49 +0500

    add tests for plain formatter

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -10,14 +10,26 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('Two json-files comparsion', () => {
+test('Two json-files stylish comparsion', () => {
   const expectedResult = readFixture('expected_file.txt');
   const result = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
   expect(result).toEqual(expectedResult);
 });
 
-test('Two yml-files comparsion', () => {
+test('Two yml-files stylish comparsion', () => {
   const expectedResult = readFixture('expected_file.txt');
   const result = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml');
+  expect(result).toEqual(expectedResult);
+});
+
+test('Two json-files plain comparsion', () => {
+  const expectedResult = readFixture('expected_plain.txt');
+  const result = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
+  expect(result).toEqual(expectedResult);
+});
+
+test('Two yml-files plain comparsion', () => {
+  const expectedResult = readFixture('expected_plain.txt');
+  const result = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain');
   expect(result).toEqual(expectedResult);
 });
