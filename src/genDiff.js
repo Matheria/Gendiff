6573c343d30c697d8eed5a0e15de081ba132c4ae
Matheria <sensenevercame@gmail.com>
2022-04-13 01:35:51 +0500

    update genDiff function

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import fileParse from './parsers.js';
-import stylishFormatter from './formatters/stylish.js';
+import changeFormatter from './formatters/index.js';
 
 const readFile = (filepath) => {
   const fullPath = path.resolve(process.cwd(), filepath);
@@ -11,34 +11,41 @@ const readFile = (filepath) => {
   return file;
 };
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileExtension1 = path.extname(filepath1);
   const fileExtension2 = path.extname(filepath2);
   const parsedFile1 = fileParse(readFile(filepath1), fileExtension1);
   const parsedFile2 = fileParse(readFile(filepath2), fileExtension2);
 
   const iter = (obj1, obj2) => {
-    const keys = Object.keys({ ...obj1, ...obj2 }).sort();
+    const keys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
 
     return keys.reduce((acc, key) => {
       const value1 = obj1[key];
       const value2 = obj2[key];
+      const isObj1HasKey = _.has(obj1, key);
+      const isObj2HasKey = _.has(obj2, key);
 
       if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
         acc.push({ key, value: iter(value1, value2), hasChildren: true });
         return acc;
       }
 
-      if (value1 === value2) {
-        acc.push({ key, status: 'unchanged', value: value1 });
+      if (isObj1HasKey && isObj2HasKey) {
+        acc.push({
+          key,
+          status: 'updated',
+          value: value2,
+          oldValue: value1,
+        });
         return acc;
       }
 
-      if (_.has(obj1, key)) {
+      if (isObj1HasKey) {
         acc.push({ key, status: 'deleted', value: value1 });
       }
 
-      if (_.has(obj2, key)) {
+      if (isObj2HasKey) {
         acc.push({ key, status: 'added', value: value2 });
       }
 
@@ -48,7 +55,7 @@ const genDiff = (filepath1, filepath2) => {
 
   const ast = iter(parsedFile1, parsedFile2);
 
-  return stylishFormatter(ast);
+  return changeFormatter(ast, formatName);
 };
 
 export default genDiff;
