1df8080354047a31437f75a5812a6c947e999e99
Matheria <sensenevercame@gmail.com>
2022-04-13 22:36:03 +0500

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2, { format }) => {
-    console.log(genDiff(filepath1, filepath2, format));
+  .action((filepath1, filepath2) => {
+    const { format } = program.opts();
+    const diff = genDiff(filepath1, filepath2, format);
+    console.log(diff);
   });
 
 program.parse();
